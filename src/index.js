6ea95e0b933ae91faa2f67c744f4d0e7710f7ead
sumitstranger <sumitrathore150598@gmail.com>
2020-04-13 17:11:58 +0530

    Modify Room List feature

--- src/index.js
+++ src/index.js
@@ -69,6 +69,9 @@ io.on('connection',(socket)=>{
             room: user.room,
             users: getUserInRoom(user.room)
         })
+        io.emit('roomListvalue',{
+            roomlist: RoomList()
+        })
         }
     })
 
